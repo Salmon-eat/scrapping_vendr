45cbbbfab4291f7763d1b88a62231128bf240ac4
Vadym Vizirov <vadymplus57@gmail.com>
2026-02-04 20:44:18 +0200

    create docker

--- app2/main.py
+++ app2/main.py
@@ -1,9 +1,12 @@
 import multiprocessing
 import queue
 import time
+from dataclasses import asdict
+from db import SessionLocal, engine, Base
 
 from playwright.sync_api import sync_playwright
 
+from app2.model import Book
 from app2.scrapping_page_url.scrapping_url import book_product_links_generator
 from app2.scrapping_product_page.prosuct_page import parce_book
 
@@ -13,16 +16,41 @@ db_queue = queue.Queue()
 
 def worker(result):
     with sync_playwright() as play:
-        browser = play.chromium.launch(headless=False)
+        browser = play.chromium.launch(headless=True)
+        context = browser.new_context()
         page = browser.new_page()
         for url in result:
             try:
                 page.goto(url, timeout=30000)
                 book = parce_book(page)
-                print(f"Воркер: {book.price}")
+                db_queue.put(book)
             except Exception as e:
                 print(f"Error - {e}")
+        context.close()
         browser.close()
+
+
+def db_writer(db_queue):
+    Base.metadata.create_all(bind=engine)
+    session = SessionLocal()
+    try:
+        while True:
+            book_data = db_queue.get()
+            if book_data is None:
+                break
+            try:
+                new_book = Book(**asdict(book_data))
+                session.add(new_book)
+                session.commit()
+            except Exception as e:
+                session.rollback()
+            finally:
+                db_queue.task_done()
+    except Exception as e:
+        print(f"Error {e}")
+    finally:
+        session.close()
+
 
 
 def main():
@@ -53,4 +81,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    manager = multiprocessing.Manager()
+    db_queue = manager.Queue()
+    writer_process = multiprocessing.Process(target=db_writer, args=(db_queue,))
+    writer_process.start()
+
--- app2/model.py
+++ app2/model.py
@@ -1,13 +1,15 @@
-from dataclasses import dataclass
+from sqlalchemy import Column, Integer, String, Float, Text, JSON
+from db import Base
 
+class Book(Base):
+    __tablename__ = "books"
 
-@dataclass
-class Book:
-    title: str
-    category: str
-    price: float
-    rating: int
-    stock_availability: str
-    image_url: str
-    description: str
-    product_information: dict
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    category = Column(String)
+    price = Column(Float)
+    rating = Column(Integer)
+    stock_availability = Column(String)
+    image_url = Column(Text)
+    description = Column(Text)
+    product_information = Column(JSON)
